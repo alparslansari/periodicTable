96807f4c81dd672165a94b56499a3bd4f28ff9d7
Alparslan Sari <asari@udel.edu>
2016-10-28 09:50:54 -0400

    initial commit

---
+++ PTable.h
@@ -0,0 +1,14 @@
+#ifndef PTABLE_H
+#define PTABLE_H
+
+#include <map>
+#include <string>
+using namespace std;
+
+class PTable{
+public:
+   void help();
+   int getElementNo(string key);
+};
+
+#endif // VALIDATION_H
---
+++ sample.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include "PTable.h"
+using namespace std;
+
+int main(void)
+{
+        PTable pt;
+        cout << pt.getElementNo("Mn") << endl;
+}
+
